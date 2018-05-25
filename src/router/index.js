03e7173b2908d8d799a09bd52712ce75eb1fa25d
junierice <383483016@qq.com>
2018-05-25 18:36:20 +0800

    0525

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import IndexComponent from "@/components/index/index.vue";
+
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path:'/',
+      redirect:'/index'
+    },
+    {
+      path:"/index",
+      component:IndexComponent,
+    },
   ]
 })
+
