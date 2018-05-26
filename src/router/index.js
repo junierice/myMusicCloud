89007664ff8ff7cc4b158910165b64045c676f1e
Juni <yxf151330@utdallas.edu>
2018-05-27 02:23:34 +0800

    0527morning

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
-import IndexComponent from "@/components/index/index.vue";
-
-
+import IndexComponent from '../components/index/index.vue'
 
 Vue.use(Router)
 
@@ -15,13 +13,12 @@ export default new Router({
     //   component: HelloWorld
     // },
     {
-      path:'/',
-      redirect:'/index'
+      path: '/',
+      redirect: '/index'
     },
     {
-      path:"/index",
-      component:IndexComponent,
-    },
+      path: '/index',
+      component: IndexComponent
+    }
   ]
 })
-
