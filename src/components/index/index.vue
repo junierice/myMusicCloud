86f2b63f0bfc762466f895dd1bf76ce87546d1d2
Juni <yxf151330@utdallas.edu>
2018-05-27 23:50:53 +0800

    0528morning navbar-ing

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,17 +1,16 @@
 <template>
     <div>
         <brand></brand>
-        <!-- <mt-navbar>
-            <mt-tab-item id="1">选项一</mt-tab-item>
-            <mt-tab-item id="2">选项二</mt-tab-item>
-            <mt-tab-item id="3">选项三</mt-tab-item>
-        </mt-navbar> -->
+        <mt-navbar v-model="selected">
+            <mt-tab-item id="songReco">推荐音乐</mt-tab-item>
+            <mt-tab-item id="songHot">热歌榜</mt-tab-item>
+            <mt-tab-item id="songSearch">搜索</mt-tab-item>
+        </mt-navbar>
     </div>
 </template>
 
 <script>
 import brand from '../brand/brand.vue'
-// import { Navbar, TabItem } from 'mint-ui'
 export default {
   name: 'index',
   components: {brand}
