6128a3ac59e803cce76085505c2a07ee04adab83
Juni <yxf151330@utdallas.edu>
2018-05-27 22:22:18 +0800

    0527night

---
+++ src/components/brand/brand.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="m-hometop">
+        <div class="topfl">
+            <img src="../../../img/musicLogo.png" alt="Music Logo"/>
+            <img src="../../../img/brand.png" alt="我的好音乐" />
+        </div>
+        <div class="topfr">
+            下载APP
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'brand'
+}
+</script>
+
+<style scoped>
+.m-hometop {
+    width: 100%;
+    height: 64px;
+    background-color: #d43c33;
+    color: white;
+    display: -webkit-flex; /* Safari */
+    display: flex;
+    align-items: center; /* 垂直居中 */
+    justify-content: space-between;
+}
+.topfl{
+    height: 25px;
+    width: 170px;
+    margin: 0 3px;
+}
+.topfr{
+    height: 30px;
+    width: 80px;
+    margin: 0 8px;
+    border: 1px solid white;
+    border-radius: 15px;
+    font-size: 15px;
+    line-height: 30px;
+}
+img {
+    height: 100%;
+}
+</style>
---
+++ src/components/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <brand></brand>
+        <!-- <mt-navbar>
+            <mt-tab-item id="1">选项一</mt-tab-item>
+            <mt-tab-item id="2">选项二</mt-tab-item>
+            <mt-tab-item id="3">选项三</mt-tab-item>
+        </mt-navbar> -->
+    </div>
+</template>
+
+<script>
+import brand from '../brand/brand.vue'
+// import { Navbar, TabItem } from 'mint-ui'
+export default {
+  name: 'index',
+  components: {brand}
+}
+</script>
+
+<style>
+
+</style>
