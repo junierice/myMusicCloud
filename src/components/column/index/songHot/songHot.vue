e3bb265471f1901c84f5d372a180f85620173a23
Yushen <383483016@qq.com>
2018-06-29 14:29:27 +0800

    son-father-commute

--- src/components/column/index/songHot/songHot.vue
+++ src/components/column/index/songHot/songHot.vue
@@ -1,16 +1,40 @@
 <template>
     <div>
       <img src="../../../../../img/btf-pic.png"/>
+      <tst v-bind:param="tt" v-bind:songList="songHotList"></tst>
       <songBlocksRank></songBlocksRank>
       <mt-cell title="下载" class = "mt-cell-download" to ="https://github.com/junierice/myMusicCloud.git"></mt-cell>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+import tst from './sstest'
 import songBlocksRank from '../../../block/songBlocksRank'
 export default {
   name: 'songHot',
-  components: {songBlocksRank}
+  components: {songBlocksRank, tst},
+  data () {
+    return {
+      tt: 'fang',
+      songHotList: []
+    }
+  },
+  created () {
+    this.init()
+  },
+  methods: {
+    init: function () {
+      axios.get('api/top/list?idx=1')
+        .then(res => {
+          let a = [...res.data.playlist.tracks]
+          this.songHotList = a.slice(0, 20)
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }
+  }
 }
 </script>
 
---
+++ src/components/column/index/songHot/sstest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ param }}
+    <p v-for="song in songList" :key="song.id">
+      {{ song.name }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: {
+    param: String,
+    songList: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
